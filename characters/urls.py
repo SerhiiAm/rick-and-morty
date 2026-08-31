510f8688e50a1a5b56b8764610f538b562c5a60e
Serhii <sergei.amvarcumov@gmail.com>
2026-08-31 20:58:04 +0200

    Added drf & 1st view -random character implemented

---
+++ characters/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from characters.views import get_random_character_view
+
+app_name = "characters"
+urlpatterns = [
+    path("character/random/", get_random_character_view, name="character-random"),
+]
